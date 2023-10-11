3a40e841fcd3dd3485bc51f27de1667d3c5e45aa
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-10-10 20:52:47 -0700

    Finished categories section in settings page

--- src/components/Settings/Account.tsx
+++ src/components/Settings/Account.tsx
@@ -3,14 +3,14 @@ import CategoryCard from "./CategoryCard";
 import { MdLaptop, MdMarkUnreadChatAlt } from "react-icons/md";
 import { IoLanguage } from "react-icons/io5";
 import { BsFillPersonFill } from "react-icons/bs";
-import { HiOutlineExternalLink } from "react-icons/hi";
+import { HiOutlineExternalLink, HiOutlineLogout } from "react-icons/hi";
 import ReactFlagsSelect from "react-flags-select";
 
 const Account = () => {
   const [selectedCountry, setSelectedCountry] = useState<string>("");
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-start gap-5 p-4">
+    <div className="w-full h-full flex flex-col items-center justify-start gap-5 px-4 py-8">
       <CategoryCard
         name="Auto-activate cashback"
         icon={<MdLaptop className="text-black-main text-2xl flex-shrink-0" />}
@@ -52,9 +52,23 @@ const Account = () => {
             Contrary to popular belief, Lorem Ipsum is not simply random text.
             It has roots in a piece of classical Latin literature from
           </p>
-          <button className="h-[56px] sm:w-[240px] w-full flex items-center justify-center gap-3 text-brand-main sm:text-xl text-lg font-semibold border-2 border-black-greish rounded-md">
+          <button className="h-[56px] sm:w-[240px] w-full flex items-center justify-center gap-3 text-brand-main sm:text-xl text-lg font-semibold border-2 border-black-greish rounded-lg">
             Manage Account
             <HiOutlineExternalLink className="text-brand-main text-2xl flex-shrink-0" />
+          </button>
+        </div>
+      </div>
+      <div className="w-full flex items-center justify-between">
+        <button className="h-[56px] sm:w-[180px] w-full flex items-center justify-center gap-3 text-[#FC3400] sm:text-xl text-lg font-semibold border-2 border-black-greish rounded-lg">
+          <HiOutlineLogout className="text-[#FC3400] text-2xl flex-shrink-0" />
+          Logout
+        </button>
+        <div className="flex items-center justify-center gap-4">
+          <button className="sm:w-[150px] w-full h-[56px] flex items-center justify-center text-black-secondary sm:text-xl text-lg font-semibold bg-transparent border-2 border-[#E3E3E3] rounded-[8px]">
+            Discard
+          </button>
+          <button className="sm:w-[150px] w-full h-[56px] flex items-center justify-center text-black-secondary sm:text-xl text-lg font-semibold bg-brand-main rounded-[8px]">
+            Save
           </button>
         </div>
       </div>
--- src/components/Settings/Categories.tsx
+++ src/components/Settings/Categories.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import CategoryCard from "./CategoryCard";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const categories = [
+    "All",
+    "Health & Beauty",
+    "Apparel",
+    "Electronics corporation",
+    "Travel Company",
+  ];
+  return (
+    <div className="w-full h-full flex flex-col items-start justify-start gap-5 px-4 py-8">
+      <p className="text-black-main text-2xl font-semibold">
+        Shopping Category
+      </p>
+      <p className="xl:w-[60%] lg:w-[70%] w-full text-black-off text-lg font-medium font-manrope">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text
+      </p>
+      {categories.map((item: any, index: number) => (
+        <CategoryCard key={index} name={item} />
+      ))}
+      <button className="h-[67px] w-full flex items-center justify-start gap-4 text-xl text-brand-main font-semibold px-4 border-2 border-black-greish rounded-md">
+        <AiOutlinePlus className="text-2xl text-brand-main flex-shrink-0" />
+        Add Category
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
